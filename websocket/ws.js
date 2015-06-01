49655ce27d3f0bf08e4f6eddaa292b9e2e88d898
soneway <250084439@qq.com>
2015-06-01 22:22:27 +0800

    cm

--- websocket/ws.js
+++ websocket/ws.js
@@ -1,23 +1,19 @@
-﻿var conns = new Array();  
- var ws = require('websocket');  
- var server = ws.createServer();  
- server.addListener('connection', function(conn)
- {  
-     console.log('connection....');  
-     conns.push(conn);  
-     conn.addListener('message',function(msg)
-	 {  
-         console.log(msg);  
-         for(var i=0; i<conns.length; i++)
-		 {  
-            if(conns[i]!=conn)
-			{  
-                 conns[i].send(msg);  
-            }  
-         }  
-     });  
- });  
+﻿var conns = new Array();
+var ws = require('websocket');
+var server = ws.createServer();
+server.addListener('connection', function (conn) {
+    console.log('connection....');
+    conns.push(conn);
+    conn.addListener('message', function (msg) {
+        console.log(msg);
+        for (var i = 0; i < conns.length; i++) {
+            if (conns[i] != conn) {
+                conns[i].send(msg);
+            }
+        }
+    });
+});
 
- server.listen(8080);  
- console.log('running......'); 
+server.listen(8080);
+console.log('running......');
                                  
